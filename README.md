3ccf67358fbfb727398e5fc63e02b53d238e13ba
Dennis Elser <dennis@backtrace.de>
2019-06-27 20:23:02 +0200

    add gif, update readme, fix code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # IDAPyHelper
+
+IDAPyHelper is a plugin for the Interactive Disassembler that helps writing IDAPython scripts and plugins.
+
+It does so by acquiring all names accessible via IDAPython and making them available as a browsable list that can be sorted, scanned (Alt-T) and filtered (Ctrl-F) arbitrarily. Double clicking a list entry opens a separate view that displays the item's docstring, if  available.
+
+![IDAPyHelper animated gif](/rsrc/pyhelper.gif?raw=true)
--- idapyhelper.py
+++ idapyhelper.py
@@ -1,50 +1,11 @@
-import idaapi
+import ida_kernwin, ida_diskio
+import os
 
 __author__ = "Dennis Elser"
 
-class PyHelperChooser(idaapi.Choose2):
-    def __init__(self, title, nb=5):
-        idaapi.Choose2.__init__(self,
-                        title,
-                        [ ["Module", 10], ["Symbol", 20], ["Documentation", 30] ],
-                        width=30, height=6)
-        self.items = []
-        for symbol in dir(idaapi):
-            try:
-                attr = getattr(idaapi, symbol)
-                docstr = attr.__doc__
-                module = attr.__module__
-            except AttributeError as ae:
-                docstr = ""
-                module = ""
-
-            self.items.append([module,
-                symbol,
-                docstr.lstrip() if docstr else ""])
-        self.icon = 0
-
-    def OnGetLine(self, n):
-        return self.items[n]
-
-    def OnGetSize(self):
-        return len(self.items)
-
-    def OnSelectLine(self, n):
-        module = self.items[n][0]
-        symbol = self.items[n][1]
-        docstring = self.items[n][2]
-        postfix = " (%s)" % module if len(module) else ""
-        if not len(docstring):
-            idaapi.msg("No documentation available for \"%s\"\n" % symbol)
-        else:
-            f = DocstringViewer("%s%s" % (symbol, postfix), docstring)
-            f.modal = False
-            f.openform_flags = idaapi.PluginForm.FORM_TAB
-            f, args = f.Compile()
-            f.Open()
-        return (idaapi.Choose.NOTHING_CHANGED, )
-
-class DocstringViewer(idaapi.Form):
+# --------------------------------------------------------------------------
+class DocstringViewer(ida_kernwin.Form):
+    """A form that displays a docstring."""
     def __init__(self, title, docstr):
         idaapi.Form.__init__(self,
 ("BUTTON YES NONE\n"
@@ -53,9 +14,70 @@ class DocstringViewer(idaapi.Form):
 "%s\n\n"
 "<##Docstring##:{cbEditable}>"
 ) % title,
-{'cbEditable': idaapi.Form.MultiLineTextControl(text=docstr,
-    flags=idaapi.textctrl_info_t.TXTF_READONLY |
-    idaapi.textctrl_info_t.TXTF_FIXEDFONT)})
+{'cbEditable': ida_kernwin.Form.MultiLineTextControl(text=docstr,
+    flags=ida_kernwin.textctrl_info_t.TXTF_READONLY |
+    ida_kernwin.textctrl_info_t.TXTF_FIXEDFONT)})
+
+# --------------------------------------------------------------------------
+class PyHelperChooser(ida_kernwin.Choose2):
+    """A chooser filled with information about IDAPython bindings.
+    Output is supposed to be filtered with Ctrl-F."""
+    def __init__(self, title, nb=5):
+        ida_kernwin.Choose2.__init__(self,
+                        title,
+                        [ ["Module", 10 | ida_kernwin.Choose.CHCOL_PLAIN],
+                        ["Symbol", 20 | ida_kernwin.Choose.CHCOL_PLAIN],
+                        ["Documentation", 30 | ida_kernwin.Choose.CHCOL_PLAIN] ],
+                        width=30, height=6)
+        self.items = []
+        self.icon = 0
+        self.build_items()
+
+    def build_items(self):
+        pydir = ida_diskio.idadir("python")
+        for name in os.listdir(pydir):
+            if name.endswith(".py"):
+                name, _ = os.path.splitext(name)
+                if name not in ["init", "idaapi"]:
+                    mod = __import__(name)
+                    for symbol in dir(mod):
+                        try:
+                            attr = getattr(mod, symbol)
+                            docstr = attr.__doc__
+                            module = attr.__module__
+                        except AttributeError:
+                            docstr = ""
+                            module = ""
+                        except NameError:
+                            docstr = ""
+                            module = ""
+
+                        if not module.startswith("_"):
+                            self.items.append((module,
+                                symbol,
+                                docstr.strip() if docstr else ""))
+
+    def OnGetLine(self, n):
+        module, symbol, docstring = self.items[n]
+        return [module,
+            symbol,
+            docstring.replace("\n", " ")]
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def OnSelectLine(self, n):
+        module, symbol, docstring = self.items[n]
+        postfix = " (%s)" % module if len(module) else ""
+        if not len(docstring):
+            ida_kernwin.msg("No documentation available for \"%s\"\n" % symbol)
+        else:
+            f = DocstringViewer("%s%s" % (symbol, postfix), docstring)
+            f.modal = False
+            f.openform_flags = ida_kernwin.PluginForm.FORM_TAB
+            f, args = f.Compile()
+            f.Open()
+        return (ida_kernwin.Choose.NOTHING_CHANGED, )
 
 try:
     pyhelper
